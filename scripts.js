52b8061096e508a02a58c48048fe071fbebf3a56
André Almeida <almeida.ae@outlook.com>
2024-02-05 20:58:00 -0300

    New Changes

--- scripts.js
+++ scripts.js
@@ -3,23 +3,23 @@
 const key = "bf78933cfbec8f1ae1966279fd7685dc"
 
 
-function colocarDadosNaTerla(dados) {
-    //console.log(dados)
+function colocarDadosNaTela(dados) {
+    console.log(dados)
     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
-    document.querySelector(".temp").innerHTML = math.floor(dados.main.temp) + "°C"
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "C°"
     document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
-    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
-    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.dados.weather[0].icon}.png`
-
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "% de humidade"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+    
 }
+
 
 
 async function buscarCidade(cidade) {
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
-    colocarDadosNaTerla(dados)
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
+    colocarDadosNaTela(dados)
 
 }
-
 
 function cliqueiNoBotao() {
     const cidade = document.querySelector(".input-cidade").value
